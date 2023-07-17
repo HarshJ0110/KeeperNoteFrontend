0017ec6905f202667ca1143afbf6be8aa1920b5c
harsh <harshjain0461@gmail.com>
2023-07-17 11:22:04 +0530

    Keeper Note frontend

--- src/App.js
+++ src/App.js
@@ -1,24 +1,36 @@
-import logo from './logo.svg';
+import React, { useContext } from 'react'
+import noteContext from "../src/context/notes/noteContext"
 import './App.css';
-
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Navbar from './components/Navbar';
+import Home from './components/Home';
+import About from './components/About';
+import Alert from './components/Alert';
+import NavModal from './components/NavModal';
+import SignIn from './components/SignIn';
+import Login from './components/Login';
+import HeroBanner from './components/HeroBanner';
 function App() {
+  const context = useContext(noteContext);
+    const {openNavModal, alert} = context;
+    const Host = process.env.REACT_APP_HOST
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      
+        <Router>
+        {openNavModal && <NavModal />}
+          <Navbar />
+          <Alert alert={alert}/>
+          <Routes>
+            <Route exact path="/" element={<Home/>} />
+            <Route exact path="/keeper" element={<HeroBanner/>} />
+            <Route exact path="/about" element={<About />} />
+            <Route exact path="/signin" element={<SignIn Host={Host}/>} />
+            <Route exact path="/login" element={<Login Host={Host}/>} />
+          </Routes>
+        </Router>
+    </>
+
   );
 }
 
